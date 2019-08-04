8108843cb8bb1112faf9ed9be7716ab4d60f8033
Adarsh Sharma <adarshsharma2692@gmail.com>
2019-08-04 13:02:48 +0530

    Adding todo body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import './App.css';
+import Body from "./components/Body";
 
 const App = () => {
   return (
     <div className="todo-container">
       <Header />
+      <Body />
     </div>
   );
 };
